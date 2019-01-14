42f47eacefb5d507117fc546736c859a20c9e88a
AlexTsymball <alya.tsymbal@gmail.com>
2019-01-14 16:24:38 +0200

    User upgrade

--- src/main/java/Entity/UserEntity.java
+++ src/main/java/Entity/UserEntity.java
@@ -1,20 +1,26 @@
 package Entity;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import jdk.jfr.Name;
+
+import javax.persistence.*;
 
 @Entity
+@Table(name = "User")
 public class UserEntity {
+
     @Id
     @GeneratedValue(strategy=GenerationType.AUTO)
+    @Column(name = "chlen")
     private Long id;
+
+
+    @Column(name = "name")
     private String name;
 
     public UserEntity() {}
 
-    public UserEntity(String name) {
+    public UserEntity(String name)
+    {
         this.name = name;
     }
 }
