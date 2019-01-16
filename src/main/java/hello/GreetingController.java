02e3bcfd821c7d90246347b5e1c6e7513174069b
AlexTsymball <alya.tsymbal@gmail.com>
2019-01-16 15:38:33 +0200

    qwert

--- src/main/java/hello/GreetingController.java
+++ src/main/java/hello/GreetingController.java
@@ -10,13 +10,13 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class GreetingController {
 
-//    UserServiceImpl userService;
+    UserServiceImpl userService;
 
     @GetMapping("/greeting")
     public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
         String t = "dfgsdf";
-//        User user = new User("df");
-//        userService.addUser(user);
+        User user = new User("df");
+        userService.addUser(user);
         model.addAttribute("name", name);
         model.addAttribute("t", t);
         return "greeting";
