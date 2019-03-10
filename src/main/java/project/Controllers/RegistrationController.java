8e86ff6df6302c233acb5604f52d018855271086
StelmakhAndrew <42778039+StelmakhAndrew@users.noreply.github.com>
2019-03-10 19:10:21 +0200

    form in jsp

--- src/main/java/project/Controllers/RegistrationController.java
+++ src/main/java/project/Controllers/RegistrationController.java
@@ -1,4 +1,27 @@
 package project.Controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+import project.Entity.User;
+
+
+@Controller
 public class RegistrationController {
+
+    @RequestMapping(value = "/registration", method = RequestMethod.GET)
+    public ModelAndView registration() {
+        System.out.println("registration");
+        return new ModelAndView("registration","registration",new User());
+    }
+
+    @RequestMapping(value = "/submit", method = RequestMethod.POST)
+    public String submit(@ModelAttribute("user")User user, ModelMap model) {
+        System.out.println("DONE");
+        return "submit";
+    }
 }
