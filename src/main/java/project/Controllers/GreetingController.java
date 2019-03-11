f6147d2999f28b8a8b9e05f3b11ea9237c34f1c4
AlexTsymball <alya.tsymbal@gmail.com>
2019-03-11 18:50:42 +0200

    finish registration

--- src/main/java/project/Controllers/GreetingController.java
+++ src/main/java/project/Controllers/GreetingController.java
@@ -13,15 +13,14 @@ import project.dao.CustomerDAO;
 public class GreetingController {
 
     @GetMapping("/greeting")
-    public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
-        ApplicationContext context = new ClassPathXmlApplicationContext("Spring-Module.xml");
-
-        CustomerDAO customerDAO = (CustomerDAO) context.getBean("customerDAO");
-        User customer = new User(1L, "mkyong");
-        customerDAO.insert(customer);
-
-        User customer1 = customerDAO.findByCustomerId(1);
-        System.out.println(customer1);
+    public String greeting(@RequestParam(name="name", required=false,
+            defaultValue="World") String name, Model model) {
+//        ApplicationContext context = new ClassPathXmlApplicationContext("Spring-Module.xml");
+//        CustomerDAO customerDAO = (CustomerDAO) context.getBean("customerDAO");
+//        User customer = new User(1L, "mkyong");
+//        customerDAO.insert(customer);
+//        User customer1 = customerDAO.findByCustomerId(1);
+//        System.out.println(customer1);
         return "greeting";
     }
 
