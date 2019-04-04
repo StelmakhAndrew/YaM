cd9018d7a32291160498001f7bdd0664e12797cb
AlexTsymball <alya.tsymbal@gmail.com>
2019-04-04 16:11:08 +0300

    bookAdd view finish

---
+++ src/main/resources/static/js/js.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+
+    function readURL(input) {
+        if (input.files && input.files[0]) {
+            var reader = new FileReader();
+
+            reader.onload = function (e) {
+                $('#blah').attr('src', e.target.result);
+                var img = document.getElementById('cancel');
+                img.style.display = 'inline';
+            }
+
+            reader.readAsDataURL(input.files[0]);
+        }
+    }
+
+    $("#picture").change(function(){
+        console.log("console");
+        readURL(this);
+
+    });
+});
